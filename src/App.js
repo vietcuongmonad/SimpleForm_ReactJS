f55c2fc060f8f177e60b6d770780fe49bbafebc8
Viet Cuong <hosyvietcuong@gmail.com>
2021-07-07 16:53:29 +0700

    Create a Simple Form with React JS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            firstName: "",
+            lastName: "",
+            isFriendly: false,
+            gender: "",
+            favColor: "Blue",
+            description: ""
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event) {
+        let {name, value, type, checked} = event.target
+        type === "checkbox" ?
+            this.setState({
+                [name]: checked
+            }) :
+            this.setState({
+                [name]: value
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type="text" name="firstName" placeholder="First Name" onChange={this.handleChange} />
+                    <input type="text" name="lastName" placeholder="Last Name" onChange={this.handleChange} />
+                    <br/>
+                    <textarea name="description" value={this.state.description} onChange={this.handleChange}/>
+                    <br/>
+                    <label>
+                        <input
+                            type="checkbox"
+                            name="isFriendly"
+                            checked={this.state.isFriendly}
+                            onChange={this.handleChange}
+                            />
+                        Is Friendly?
+                    </label>
+
+                    <br/>
+                    <label>
+                        <input
+                            type="radio"
+                            name="gender"
+                            value="male"
+                            onClick={this.handleChange}
+                        /> Male
+                    </label>
+
+                    <br/>
+                    <label>
+                        <input
+                            type="radio"
+                            name="gender"
+                            value="female"
+                            onClick={this.handleChange}
+                        /> Female
+                    </label>
+
+                    <br />
+                    <label> Favourite color: </label>
+                    <select value={this.state.favColor} name="favColor" onChange={this.handleChange} >
+                        <option value="Blue">Blue</option>
+                        <option value="Red">Red</option>
+                        <option value="Green">Green</option>
+                    </select>
+
+                    <h1>{this.state.firstName} {this.state.lastName}</h1>
+                    <p> Other details: {this.state.description} </p>
+
+                    <h2>You are a {this.state.gender}</h2>
+                    <h2>Your favourite color is: {this.state.favColor}</h2>
+                    <button>Submit</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default App;
